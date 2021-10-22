68fda44699ce25fe3b6e2adab23b83c836c78ced
fh-kazemi <fh.kazemi87@gmail.com>
2021-10-22 23:37:50 +0330

    Improve Properties To Customer Model By fh.kazemi

--- src/main/java/com/kazemi/home_services_company/model/users/Customer.java
+++ src/main/java/com/kazemi/home_services_company/model/users/Customer.java
@@ -1,18 +1,34 @@
 package com.kazemi.home_services_company.model.users;
 
+import com.kazemi.home_services_company.enums.UserRole;
+import com.kazemi.home_services_company.model.homeservices.Comments;
+import com.kazemi.home_services_company.model.homeservices.Orders;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author fh.kazemi
  **/
 
 @Entity
+@AllArgsConstructor
 @Setter
 @Getter
-@ToString
 public class Customer extends User {
+
+    @OneToMany(mappedBy = "customer")
+    private List<Comments> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "customer")
+    private List<Orders> orders = new ArrayList<>();
+
+    public Customer() {
+        this.setUserRole(UserRole.CUSTOMER);
+    }
 }
