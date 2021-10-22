0130f7fe3b12331d5e609d7d3d33a7aa3859ceab
fh-kazemi <fh.kazemi87@gmail.com>
2021-10-22 23:40:37 +0330

    Improve Properties To Expert Model By fh.kazemi

--- src/main/java/com/kazemi/home_services_company/model/users/Expert.java
+++ src/main/java/com/kazemi/home_services_company/model/users/Expert.java
@@ -1,10 +1,17 @@
 package com.kazemi.home_services_company.model.users;
 
-import lombok.*;
+import com.kazemi.home_services_company.enums.UserRole;
+import com.kazemi.home_services_company.model.homeservices.Comments;
+import com.kazemi.home_services_company.model.homeservices.ExpertOffers;
+import com.kazemi.home_services_company.model.homeservices.Orders;
+import com.kazemi.home_services_company.model.homeservices.SubService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Lob;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author fh.kazemi
@@ -12,10 +19,8 @@ import javax.persistence.Lob;
 
 @Entity
 @AllArgsConstructor
-@NoArgsConstructor
 @Setter
 @Getter
-@ToString
 public class Expert extends User {
 
     @Lob
@@ -24,4 +29,30 @@ public class Expert extends User {
 
     @Column(name = "expert_score")
     private Integer score;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+    @JoinTable(name = "expert_services",
+            joinColumns = {
+                    @JoinColumn(name = "expert_id", referencedColumnName = "id",
+                            nullable = false, updatable = false)},
+            inverseJoinColumns = {
+                    @JoinColumn(name = "subService_id", referencedColumnName = "id",
+                            nullable = false, updatable = false)})
+    private List<SubService> subServices = new ArrayList<>();
+
+    @OneToMany(mappedBy = "expert")
+    private List<ExpertOffers> offers = new ArrayList<>();
+
+    @OneToMany(mappedBy = "expert")
+    private List<Comments> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "expert")
+    private List<Orders> orders = new ArrayList<>();
+
+    @Column(name = "field")
+    private String field;
+
+    public Expert() {
+        this.setUserRole(UserRole.EXPERT);
+    }
 }
